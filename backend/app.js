051e35fac29347b78884803f30aacba29f0f348a
Zhehai Zhang <zhehaizhang1@gmail.com>
2020-09-18 21:37:12 -0400

    server stuffs

---
+++ backend/app.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const cors = require('cors');
+
+const morgan = require('morgan');
+
+const app = express();
+
+//Routes
+
+
+app.use(morgan('dev'));
+app.use(express.urlencoded({extended:false}));
+app.use(express.json());
+app.use(cors());
+
+//Initialize Routes
+
+
+
+
+
+//Errors
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+})
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        error: {
+            message: error.message
+        }
+    })
+})
+
+module.exports = app;

62eb19ceeaffa820f43cee0ef1b2b9f2c2e9b599
Nithin Muthukumar <kathrikai30201094@gmail.com>
2020-09-19 14:25:10 -0400

    adding schedule and abstracting to services

--- backend/api/routes/zoom.js
+++ backend/api/routes/zoom.js
@@ -1,66 +1,14 @@
 const express = require('express');
 const zoomRouter = express.Router();
-const querystring = require('querystring');
-const {CLIENT_ID} = require("../../config");
-const {CLIENT_SECRET} = require("../../config");
+const {assignCode} =require('../../services/zoom');
 var request = require("request");
-
-zoomRouter.get('/create',async (req,res)=>{
-    let code= req.query.code;
-    console.log(code);
-    console.log(CLIENT_ID + ':' + CLIENT_SECRET);
-    var authOptions = {
-        method: 'POST',
-        url: 'https://zoom.us/oauth/token',
-        qs: {
-            grant_type: 'authorization_code',
-            //The code below is a sample authorization code. Replace it with your actual authorization code while making requests.
-            code: code,
-            //The uri below is a sample redirect_uri. Replace it with your actual redirect_uri while making requests.
-            redirect_uri: 'http://134.209.168.108:3000/zoom/create/'
-        },
-        headers: {
-            /**The credential below is a sample base64 encoded credential. Replace it with "Authorization: 'Basic ' + Buffer.from(your_app_client_id + ':' + your_app_client_secret).toString('base64')"
-             **/
-            Authorization: 'Basic ' + Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64')
-        }
-
-    };
-    request(authOptions, function(error, response, body) {
-        let token = JSON.parse(body).access_token;
-        console.log(token);
-        const meetOptions = {
-            method:"POST",
-            url:"https://api.zoom.us/v2/users/me/meetings",
-            headers: {
-                Authorization: "Bearer "+token,
-                "Content-Type":"application/json"
-            },
-            json:{
-                topic: "appointment",
-                start_time: "2020-09-19T12:28:00Z",
-                duration: 30,
-                timezone: "UTC",
-                password: "nithin",
-                agenda: "Checkup",
-                settings: {
-                    host_video: true,
-                    participant_video: true,
-                    join_before_host: false,
-                    mute_upon_entry: true,
-                    enforce_login: false,
-                    registrants_email_notification: true
-                }
-            }
-
-        };
-        request(meetOptions,function(error,response,body){
-            console.log(body);
-
-        });
-    });
-
-
+//takes in email, and code
+//associates auth token with email
+zoomRouter.post('/create',async (req,res)=>{
+    const {code} =req.body;
+    assignCode(code,req.userData);
+    res.send(200);
 });
+
 
 module.exports=zoomRouter;
--- backend/services/zoom.js
+++ backend/services/zoom.js
@@ -1,3 +1,36 @@
-function getCode() {
+const {CLIENT_ID} = require("../../config");
+const {CLIENT_SECRET} = require("../../config");
+const client = require('./database').client;
+function assignCode(code,email) {
+    var authOptions = {
+        method: 'POST',
+        url: 'https://zoom.us/oauth/token',
+        qs: {
+            grant_type: 'authorization_code',
+            //The code below is a sample authorization code. Replace it with your actual authorization code while making requests.
+            code: code,
+            //The uri below is a sample redirect_uri. Replace it with your actual redirect_uri while making requests.
+            redirect_uri: 'http://134.209.168.108:3000/zoom/create/'
+        },
+        headers: {
+            /**The credential below is a sample base64 encoded credential. Replace it with "Authorization: 'Basic ' + Buffer.from(your_app_client_id + ':' + your_app_client_secret).toString('base64')"
+             **/
+            Authorization: 'Basic ' + Buffer.from(CLIENT_ID + ':' + CLIENT_SECRET).toString('base64')
+        }
 
+    };
+    request(authOptions,function(error,response,body){
+
+        let token = JSON.parse(body).access_token;
+        // add query that takes token and assigns it to the email.
+        //client.query()
+
+    });
+}
+function retrieve_auth(){
+
+}
+
+module.exports = {
+    assignCode: assignCode
 }

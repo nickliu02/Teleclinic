4e840a0e885a836b2088cae34175c8b6bfeaf97f
Zhehai Zhang <zhehaizhang1@gmail.com>
2020-09-19 01:09:25 -0400

    changed outta typescript

--- backend/services/auth.js
+++ backend/services/auth.js
@@ -1,24 +1,32 @@
-import {KEY} from '../config.js';
-import { client } from './database.service';
+
+const KEY = require('../config').KEY;
+const client = require('./database').client;
+
+
 
 const jwt = require('jsonwebtoken');
 
-export const register = (email, first_name,last_name, password, phone_number, health_card_number) => client.query(
+const register = (email, first_name,last_name, password, phone_number, health_card_number) => client.query(
     'INSERT INTO users(email, first_name, last_name, password, phone_number, health_card_number) VALUES ($1,$2,$3,$4,$5, $6) RETURNING email',
     [email,first_name,last_name,password,phone_number,health_card_number]
 )
     .then(res => generateToken(res.rows[0].email))
     .catch(e => e);
 
-export const login = (email,password) => client.query(
+const login = (email,password) => client.query(
     'SELECT * FROM users where email = $1 AND password = $2',
     [email,password]
 )
     .then(res => generateToken(res.rows[0].username))
     .catch(e => e);
 
-export const generateToken = (email) => jwt.sign(
+const generateToken = (email) => jwt.sign(
     { email: email},
     KEY,
     {expiresIn: "24h"}
 );
+
+module.exports = {
+    register: register,
+    login: login
+}

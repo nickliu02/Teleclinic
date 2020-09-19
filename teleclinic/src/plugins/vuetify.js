26e5a6bef754196ccfe5e80fb91e49a0488f2e0b
Nick Liu <nickliu02@gmail.com>
2020-09-18 21:34:39 -0400

    lolxd

--- teleclinic/src/plugins/vuetify.js
+++ teleclinic/src/plugins/vuetify.js
@@ -1,7 +1,19 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import { colors } from 'vuetify/lib';
 
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: "#4ABDAC", //Where u change colours
+                secondary: "FC4A1A",
+                accent: "F7B733",
+                error: colors.red.accent3,
+                success: colors.blue.base
+            }
+        }
+    },
 });

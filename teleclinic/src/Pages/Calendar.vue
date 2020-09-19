46796f90ba7eff6ea5980957285f4c88bd1b3e27
Anish Aggarwal <anish.5@hotmail.com>
2020-09-18 21:58:45 -0400

    Adding Calendar

---
+++ teleclinic/src/Pages/Calendar.vue
@@ -0,0 +1,24 @@
+<template>
+    <v-app>
+        <h1>Hello World</h1>
+    </v-app>
+</template>
+
+<script>
+export default {
+  name: 'Calendar',
+
+  components: {
+
+  },
+
+  data: () => ({
+
+
+  }),
+
+  methods: {
+
+  }
+};
+</script>
--- teleclinic/src/router/index.js
+++ teleclinic/src/router/index.js
@@ -1,12 +1,16 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "../components/Home.vue";
+import Calendar from "../Pages/Calendar.vue";
 Vue.use(Router);
 
 let router = new Router({
     routes: [
       {
         path: "/", name:"Home", component: Home
+      },
+      {
+        path: "/appointments", name:"Appointments", component:Calendar
       },
 
     ],

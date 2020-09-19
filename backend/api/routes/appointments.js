da9517278a36507c7900808e9824a9b12c5221ec
Zhehai Zhang <zhehaizhang1@gmail.com>
2020-09-19 15:57:27 -0400

    oops

--- backend/api/routes/appointments.js
+++ backend/api/routes/appointments.js
@@ -2,14 +2,14 @@ const express = require('express');
 const appointmentRouter = express.Router();
 const {retrieve_zoom_auth} = require('../../services/auth')
 const {createMeeting} = require('../../services/appointments');
-appointmentRouter.post('/create',async (req,res)=>{
+appointmentRouter.post('/add',async (req,res)=>{
     const {start_time,doctor_email} =req.body;
-    createMeeting(req.body, start_time,doctor_email);
+    createMeeting(req.body, start_time,doctor_email,req.userData);
 
 
 
 });
-appointmentRouter.get('/list',async (req,res)=>{
+appointmentRouter.get('/get',async (req,res)=>{
 
 });
 

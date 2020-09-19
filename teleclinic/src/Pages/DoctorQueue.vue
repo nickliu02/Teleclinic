1ccf588585f1b18d925603af2eb7c17d4f49a2d9
Nick Liu <nickliu02@gmail.com>
2020-09-19 15:41:56 -0400

    Update DoctorQueue.vue

--- teleclinic/src/Pages/DoctorQueue.vue
+++ teleclinic/src/Pages/DoctorQueue.vue
@@ -1,15 +1,162 @@
 <template>
-  <v-app>
-      <h1><a>Doctor Queue</a></h1>
+  <v-app :style="{background: '#eee'}">
+      <v-row class="mx-auto my-auto">
+        <v-card
+          width="300"
+          class="ma-10"
+        >
+            <v-card-title class="black--text accent">
+              <h1 class="mx-auto" id="welcome">
+                Appointments
+              </h1>        
+            </v-card-title>
+
+            <v-virtual-scroll
+                    :items="appointments"
+                    :item-height="50"
+              > 
+                  <template v-slot="{ item }">
+                    <v-list-item>
+
+                      <v-list-item-content>
+                        <v-list-item-title>
+                            {{ item.first_name + " " + item.last_name}}
+
+                            <span style="font-weight: bold">{{toToDfromTimestamp(item.start)}}</span>
+
+                        </v-list-item-title>
+                        
+                      </v-list-item-content>
+
+                      <v-list-item-action>
+                        <v-btn
+                          depressed
+                          small
+                        >
+                          View
+                        </v-btn>
+                      </v-list-item-action>
+                    </v-list-item>
+                  </template>
+
+                </v-virtual-scroll> 
+
+            
+        </v-card>
+
+        <v-card
+          width="300"
+          class="ma-10"
+        >
+            <v-card-title class="black--text accent">
+              <h1 class="mx-auto" id="welcome">
+                Walk-ins
+              </h1>         
+            </v-card-title>
+
+            <v-virtual-scroll
+                    :items="walk_ins"
+                    :item-height="50"
+              > 
+                  <template v-slot="{ item }">
+                    <v-list-item>
+
+                      <v-list-item-content>
+                        <v-list-item-title>
+                            {{ item.first_name + " " + item.last_name}}
+
+                        </v-list-item-title>
+                        
+                      </v-list-item-content>
+
+                      <v-list-item-action>
+                        <v-btn
+                          depressed
+                          small
+                        >
+                          View
+                        </v-btn>
+                      </v-list-item-action>
+                    </v-list-item>
+                  </template>
+
+                </v-virtual-scroll> 
+
+            
+        </v-card>
+
+      </v-row>
+
+      
   </v-app>
 </template>
 
 <script>
 export default {
     name: "DoctorQueue",
+    data() {
+        return {
+            appointments: [
+                {
+                    id: "25",   
+                    name: "Doctor’s Appointment", 
+                    details: "Purpose of appointment", 
+                    start: Date.now(),
+                    end: Date.now() + 100000,
+                    color: "red",
+                    zoom_link: "https://www.google.com",
+                    first_name: "ayy",
+                    last_name: "lmao",
+                },
+            ],
+
+            walk_ins: [
+                {
+                    first_name: "lol",
+                    last_name: "xd",
+                    email: "lol@lol.com"
+                },
+            ],
+
+            modalInfo: {},
+            appointmentModalOpen: {},
+            walkInModalOpen: {},
+        }
+    },
     methods: {
+        toToDfromTimestamp(ts){
+            let d = new Date(ts)
+            let dL = d.toTimeString().split(":")
+            let hr = parseInt(dL[0],10)
+            let curPeriod = "AM"
+            if(hr<=23 && hr>=12){
+            curPeriod = "PM"
+            }
+            if(hr>12){
+            hr-=12
+            }
+            let h = hr.toString()
+            return h+":"+dL[1]+" "+curPeriod
+        },
+
+        openAppointmentsModal(info) {
+            this.modalInfo = info;
+            this.appointmentModalOpen = true;
+        },
+
+        openWalkInModal(info) {
+            this.modalInfo = info;
+            this.walkInModalOpen = true;
+        }
         
     },
+
+    mounted() {
+        for (let i = 0; i < 3; i++) {
+            this.appointments.push(this.appointments[0]);
+            this.walk_ins.push(this.walk_ins[0]);
+        }
+    }
     
 }
 </script>

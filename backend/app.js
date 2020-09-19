2d8477cec228442882dfbf486be15db9f847572a
Zhehai Zhang <zhehaizhang1@gmail.com>
2020-09-19 12:01:23 -0400

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -1,42 +1,39 @@
+
 const express = require('express');
 const cors = require('cors');
-
-const morgan = require('morgan');
+const morgan = require('morgan'); // Logging
 
 const app = express();
 
 //Routes
 const authRoutes = require('./api/routes/auth');
 const zoomRoutes = require('./api/routes/zoom')
+
 app.use(morgan('dev'));
-
-//app.use(express.json());
-
-var corsOptions = {
-    origin: '*',
-    optionsSuccessStatus: 200,
-  }
-app.use(cors(corsOptions));
-
+app.use(express.urlencoded({extended:false}));
 app.use(express.json());
+app.use(cors());
 
-//Initialize Routes
-
+//Initialize routes
 app.use('/auth',authRoutes);
 app.use('/zoom',zoomRoutes);
 
 
-
 //Errors
+app.use((req,res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+})
 
-
-app.use((error, req, res, next) => {
+app.use((error,req,res, next) => {
     res.status(error.status || 500);
     res.json({
         error: {
             message: error.message
         }
-    })
+    });
 })
+
 
 module.exports = app;

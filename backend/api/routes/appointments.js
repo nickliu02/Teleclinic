b4cae6bfea58de025c5d8abc34b04ac2bab1d6ae
Zhehai Zhang <zhehaizhang1@gmail.com>
2020-09-19 23:28:26 -0400

    Update appointments.js

--- backend/api/routes/appointments.js
+++ backend/api/routes/appointments.js
@@ -12,7 +12,7 @@ appointmentRouter.get('/get',checkAuth,async (req,res)=>{
     //Check if it's doctor. If it's doctor, do where statement
     const value = await isDoctor(req.userData);
     console.log(value, "this is the doctor truth value");
-    if (value.exists == true){
+    if (value == true){
         //Do doctor
         const infos = await getDoctor(req.userData);
         console.log(infos);

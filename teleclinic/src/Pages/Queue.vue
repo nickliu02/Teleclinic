581a1211fb7aad532d5f366e83bb06aa7488f62c
Nick Liu <nickliu02@gmail.com>
2020-09-19 23:55:10 -0400

    lol

--- teleclinic/src/Pages/Queue.vue
+++ teleclinic/src/Pages/Queue.vue
@@ -57,11 +57,40 @@
 
             <div style="padding: 20px">
               <h3 align="center">Your place in line is: {{place}}</h3>
-              <h3 align="center">Estimated wait time: {{waitTimeToString(waitTime)}}</h3>
+              <h3 align="center">Estimated wait time: {{waitTimeToString(getWaitTime())}}</h3>
 
               <div align="center" style="margin: 10px">
                 <v-btn class="mx-auto" color="error"> 
                   Leave queue
+                </v-btn>
+
+              </div>
+              
+            </div>
+
+        </v-card>
+
+
+        <v-card
+        v-if="!isInQueue && !hasAppointment"
+          width="600"
+          class="mx-auto my-auto"
+        >
+            <v-card-title class="black--text accent">
+              <h1 class="mx-auto" id="welcome">
+                Welcome
+              </h1>         
+            </v-card-title>
+
+            <v-divider></v-divider>
+
+            <div style="padding: 20px">
+              <h3 align="center">There are {{}} people in the queue.</h3>
+              <h3 align="center">Estimated wait time: {{waitTimeToString(getWaitTime())}}</h3>
+
+              <div align="center" style="margin: 10px">
+                <v-btn class="mx-auto" color="primary"> 
+                  Join queue
                 </v-btn>
 
               </div>
@@ -81,21 +110,23 @@ export default {
   },
 
   data: () => ({
-      hasAppointment: true,
+      hasAppointment: false,
       isInQueue: false,
       
 
       appointmentInfo: {
-
+        id: "",   
+        name: "", 
+        details: "", 
+        start: 0,
+        end: 0,
+        color: "",
+        zoom_link: "",
+        first_name: "",
+        last_name: ""
       },
 
-      queueInfo: {
-        place: 0,
-      },
-      appointmentTime: "7:00pm",
-      link: "https://www.google.com",
-      place: 10,
-      waitTime: 3200
+      place: -1,
 
   }),
 
@@ -107,7 +138,70 @@ export default {
       else {
         return Math.ceil(seconds / 3600) + " hours";
       }
+    },
+
+    getWaitTime() {
+      return 0;
+    },
+
+    async getPlaceInQueue() {
+      const info = await this.$axios.get(this.$API_URL+"/queue/place", {
+            headers: {'x-access-token': localStorage.getItem('jwt')}
+        })
+        .catch(e => {
+            console.log(e);
+        });
+
+        console.log(info);
+
+          //this.queueInfo = info.data;
+    },
+
+    async enterQueue() {
+      const info = await this.$axios.post(this.$API_URL+"/queue/enter",
+            {
+              email: localStorage.getItem('email')
+            },
+
+            {
+              headers: {'x-access-token': localStorage.getItem('jwt')
+            }
+        }).then(r=>{
+          console.log(r);
+          this.isInQueue = true;
+        }) 
+        .catch(e => {
+            console.log(e);
+        });
+
+
+        console.log(info);
+    },
+
+    async leaveQueue() {
+      const info = await this.$axios.post(this.$API_URL+"/queue/exit",
+            {
+              email: localStorage.getItem('email')
+            },
+
+            {
+              headers: {'x-access-token': localStorage.getItem('jwt')
+            }
+        }).then(r=>{
+          console.log(r);
+          this.isInQueue = true;
+        }) 
+        .catch(e => {
+            console.log(e);
+        });
+
+      console.log(info);
     }
+  },
+
+  mounted() {
+      this.getPlaceInQueue();
+      
   }
 };
 </script>

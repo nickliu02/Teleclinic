c2abcd56cb159ac3a7c16213832efaf079a6e5f7
Zhehai Zhang <zhehaizhang1@gmail.com>
2020-09-20 02:43:26 -0400

    Update appointments.js

--- backend/services/appointments.js
+++ backend/services/appointments.js
@@ -51,7 +51,7 @@ async function createMeeting(lmao,start_time,doctor_email,email){
     request(meetOptions,function(error,response,body){
         console.log(body);
         console.log(error);
-        let data = JSON.parse(body);
+        let data = body;
 
 
         // data.start_url

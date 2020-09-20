4649435449aaac22f300a7a58b25baa9f8873c0d
Nithin Muthukumar <kathrikai30201094@gmail.com>
2020-09-19 20:09:12 -0400

    email

--- backend/api/routes/zoom.js
+++ backend/api/routes/zoom.js
@@ -7,7 +7,7 @@ const checkAuth = require('./middleware/check-auth');
 //associates auth token with email
 zoomRouter.post('/create',checkAuth,async (req,res)=>{
     const code =req.body.code;
-    console.log("userData",req.userData);
+    console.log("userData",req.userData.email);
     assignCode(code,req.userData);
     res.send(200);
 });
